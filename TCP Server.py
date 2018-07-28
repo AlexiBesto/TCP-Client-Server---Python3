a4246ed30eb3899074fbcdb0a78d3cab56f89ca1
Yip Hao Yang <yiphaoyang98@gmail.com>
2018-07-28 15:56:14 +0800

    update comment

--- TCP Server.py
+++ TCP Server.py
@@ -5,17 +5,18 @@ import socket
 #Creating the socket object
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = socket.gethostname() #Host is the server IP
-port = 444 #Port to listen on 
+host = socket.gethostname()     # Host is server IP
+port = 444                      # The TCP port number this server is bound to
 
 #Binding to socket
 serversocket.bind((host, port)) #Host will be replaced/substitued with IP, if changed and not running on host
 
-#Starting TCP listener
+#listen for incoming connection
 serversocket.listen(3)
 
+#Infinite loop, each iteration handles a connection from a client
 while True:
-    #Starting the connection 
+    #Accept the connection 
     clientsocket,address = serversocket.accept()
 
     print("received connection from " % str(address))
--- TCPClient.py
+++ TCPClient.py
@@ -8,13 +8,16 @@ clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #host = '192.168.1.104'
 host = socket.gethostname()
 
+#The TCP port number of the server
 port = 444
 
-clientsocket.connect(('192.168.1.104', port)) #You can substitue the host with the server IP
+#Connect the socket object to (host, port)
+clientsocket.connect(('192.168.1.104', port))
 
-#Receiving a maximum of 1024 bytes
+#Receiving a message from the server with the maximum of 1024 bytes
 message = clientsocket.recv(1024)
 
+#Close the socket
 clientsocket.close()
 
 print(message.decode('ascii'))
